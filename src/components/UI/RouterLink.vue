eb32d1709c6461750c34fa4701e2baeb49fc9469
Cristian Sena <crisianjsena@gmail.com>
2024-07-24 18:52:22 -0300

    agrego componente

---
+++ src/components/UI/RouterLink.vue
@@ -0,0 +1,40 @@
+<script setup>
+    import { RouterLink } from 'vue-router'
+
+    defineProps({
+        to: {
+            type: String,
+            required: true
+        }
+    })
+
+</script>
+
+<template>
+    <RouterLink 
+        :to="{ name: to}" 
+        class="
+            block md:inline-block 
+            rounded 
+            bg-blue-500
+            text-white
+            text-sm
+            uppercase
+            shadow
+            px-3 
+            py-2 
+            md:mr-2 
+            cursor-pointer
+            font-bold
+            hover:bg-blue-600
+            hover:text-white
+            hover:shadow
+            transition
+            duration-300
+            ease-in-out"
+    >
+        <slot></slot> 
+    </RouterLink>
+</template>
+
+
