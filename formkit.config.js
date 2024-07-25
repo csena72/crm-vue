391913c4aa0c013fff83528d618491c759d09ee7
Cristian Sena <crisianjsena@gmail.com>
2024-07-24 21:50:34 -0300

    Agrego FormKit

---
+++ formkit.config.js
@@ -0,0 +1,18 @@
+const config = {
+    theme: 'genesis',
+    config: {
+        plugins: [
+            'inputmask',
+            'date',
+            'time',
+            'number',
+            'select',
+            'textarea',
+            'checkbox',
+            'radio',
+            'file', 
+        ]
+    },
+}
+
+export default config
--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { FormKit } from '@formkit/vue'
 import RouterLink from '../components/UI/RouterLink.vue'
 import Heading from '../components/UI/Heading.vue'
 
@@ -18,5 +19,71 @@ defineProps({
             </RouterLink>
         </div>
         <Heading>{{ title }}</Heading>
+
+        <div class="mx-auto mt-10 bg-white shadow rounded p-6 max-w-6xl">
+            <div class="mx-auto md:w-2/3 py-10 px-6">
+                <FormKit
+                    type="form"
+                >
+
+                    <FormKit
+                        type="text"
+                        label="Nombre"
+                        name="nombre"
+                        placeholder="Nombre del Cliente"
+                        validation="required|length:3,20"
+                        validation-visibility="live"
+                        :validation-messages="{ 
+                            required: '* El nombre es obligatorio', 
+                            length: 'El nombre debe ser entre 3 y 20 caracteres'
+                        }"
+                    />
+
+                    <FormKit
+                        type="text"
+                        label="Apellido"
+                        name="apellido"
+                        placeholder="Apellido del Cliente"
+                        validation="required|length:3,20"
+                        validation-visibility="live"
+                        :validation-messages="{ 
+                            required: '* El apellido es obligatorio', 
+                            length: 'El apellido debe ser entre 3 y 20 caracteres'
+                        }"
+                    />
+                    <FormKit
+                        type="text"
+                        label="Email"
+                        name="email"
+                        placeholder="Email del Cliente"
+                        validation="required|email"
+                        validation-visibility="live"
+                        :validation-messages="{ 
+                            required: '* El email es obligatorio', 
+                            email: 'El email debe ser un email válido'
+                        }"
+                    />
+                    <FormKit
+                        type="text"
+                        label="Teléfono"
+                        name="telefono"
+                        placeholder="Telefono: ej. 11 34567890"
+                        validation="required"
+                        validation-visibility="live"
+                        :validation-messages="{ 
+                            required: '* El teléfono es obligatorio', 
+                            email: 'El teléfono debe ser un número válido'
+                        }"
+                    />
+
+                </FormKit>
+            </div>
+        </div>
     </div>
 </template>
+
+<style>
+    .formkit-wrapper {
+        max-width: 100%!important;
+    }
+</style>
